6e4d008d0ccb1f824b9d80831192e4d21bbba899
aviau <alexandre@alexandreviau.net>
2024-09-14 16:11:37 -0400

    implement IterPostJson

--- api_client_iter.go
+++ api_client_iter.go
@@ -17,24 +17,19 @@ type IterResult struct {
 	Next     string
 }
 
-// IterGet allows to iterate over responses for an API endpoint that
-// supports the Flare standard paging pattern.
-func (client *ApiClient) IterGet(
-	path string,
-	params *url.Values,
+func createPagingIterator(
+	fetchPage func(from string) (*http.Response, error),
 ) iter.Seq2[*IterResult, error] {
-
-	type ScrollableResponse struct {
+	type ResponseWithNext struct {
 		Next string `json:"next"`
 	}
+
+	cursor := ""
 
 	return func(yield func(*IterResult, error) bool) {
 		for {
 			// Fire the request
-			response, err := client.Get(
-				path,
-				params,
-			)
+			response, err := fetchPage(cursor)
 			if err != nil {
 				yield(nil, err)
 				return
@@ -60,8 +55,8 @@ func (client *ApiClient) IterGet(
 			}
 
 			// Look for the next token
-			var scrollableResponse ScrollableResponse
-			if err := json.Unmarshal(body, &scrollableResponse); err != nil {
+			var responseWithNext ResponseWithNext
+			if err := json.Unmarshal(body, &responseWithNext); err != nil {
 				yield(
 					nil,
 					fmt.Errorf("failed to unmarshal response: %w", err),
@@ -74,7 +69,7 @@ func (client *ApiClient) IterGet(
 			if !yield(
 				&IterResult{
 					Response: response,
-					Next:     scrollableResponse.Next,
+					Next:     responseWithNext.Next,
 				},
 				nil,
 			) {
@@ -82,15 +77,63 @@ func (client *ApiClient) IterGet(
 			}
 
 			// Was this the last page?
-			if scrollableResponse.Next == "" {
+			if responseWithNext.Next == "" {
 				return
 			}
 
 			// Set the cursor for next page
-			if params == nil {
-				params = &url.Values{}
-			}
-			params.Set("from", scrollableResponse.Next)
+			cursor = responseWithNext.Next
 		}
 	}
+}
+
+// IterGet allows to iterate over responses for an API endpoint that
+// supports the Flare standard paging pattern.
+func (client *ApiClient) IterGet(
+	path string,
+	params *url.Values,
+) iter.Seq2[*IterResult, error] {
+	return createPagingIterator(
+		func(cursor string) (*http.Response, error) {
+			if cursor != "" {
+				if params == nil {
+					params = &url.Values{}
+				}
+				params.Set("from", cursor)
+			}
+			return client.Get(
+				path,
+				params,
+			)
+		},
+	)
+}
+
+// IterPostJson allows to iterate over responses for an API endpoint that
+// supports the Flare standard paging pattern.
+func (client *ApiClient) IterPostJson(
+	path string,
+	params *url.Values,
+	body map[string]interface{},
+) iter.Seq2[*IterResult, error] {
+	return createPagingIterator(
+		func(cursor string) (*http.Response, error) {
+			if cursor != "" {
+				body["from"] = cursor
+			}
+
+			encodedJson, err := json.Marshal(body)
+			if err != nil {
+				return nil, fmt.Errorf("failed to marshal body to JSON: %w", err)
+			}
+
+			return client.Post(
+				path,
+				params,
+				"application/json",
+				bytes.NewReader(encodedJson),
+			)
+		},
+	)
+
 }
--- api_client_iter_test.go
+++ api_client_iter_test.go
@@ -3,13 +3,15 @@
 package flareio
 
 import (
+	"encoding/json"
 	"net/http"
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestIterGetParams(t *testing.T) {
+func TestIterGet(t *testing.T) {
 	ct := newClientTest(
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			assert.Equal(t, "/leaksdb/sources", r.URL.Path)
@@ -32,6 +34,66 @@ func TestIterGetParams(t *testing.T) {
 	for result, err := range ct.apiClient.IterGet(
 		"/leaksdb/sources",
 		nil,
+	) {
+		lastPageIndex = lastPageIndex + 1
+		if lastPageIndex > 5 {
+			// We are going crazy here...
+			break
+		}
+		assert.Nil(t, err, "iter yielded an error")
+		assert.NotNil(t, result, "didn't get a result")
+		nextTokens = append(nextTokens, result.Next)
+	}
+
+	assert.Equal(t, 3, lastPageIndex, "Didn't get the expected number of pages")
+	assert.Equal(
+		t,
+		[]string{"second-page", "third-page", ""},
+		nextTokens,
+		"Didn't get the expected next tokens",
+	)
+
+}
+
+func TestIterPostJson(t *testing.T) {
+	ct := newClientTest(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, "/leaksdb/sources", r.URL.Path)
+			assert.Equal(t, "value1", r.URL.Query().Get("param1"))
+			assert.Equal(t, "application/json", r.Header.Get("Content-Type"))
+
+			type PagedRequest struct {
+				From string `json:"from"`
+			}
+			var pagedRequest PagedRequest
+			if err := json.NewDecoder(r.Body).Decode(&pagedRequest); err != nil {
+				assert.NoError(t, err, "Error decoding posted JSON")
+				return
+			}
+
+			cursor := pagedRequest.From
+			if cursor == "" {
+				w.Write([]byte(`{"next":"second-page", "items": []}`))
+			} else if cursor == "second-page" {
+				w.Write([]byte(`{"next":"third-page", "items": []}`))
+			} else {
+				w.Write([]byte(`{"next": null, "items": []}`))
+			}
+		}),
+	)
+	defer ct.Close()
+
+	lastPageIndex := 0
+	nextTokens := []string{}
+
+	for result, err := range ct.apiClient.IterPostJson(
+		"/leaksdb/sources",
+		&url.Values{
+			"param1": []string{"value1"},
+		},
+		map[string]interface{}{
+			"some_param": "hello",
+		},
 	) {
 		lastPageIndex = lastPageIndex + 1
 		if lastPageIndex > 5 {
