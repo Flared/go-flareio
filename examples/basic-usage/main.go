4294756ea5e874a8d7020567aac47a1b95160802
aviau <alexandre@alexandreviau.net>
2024-09-15 12:29:15 -0400

    README: update basic usage example

--- examples/basic-usage/main.go
+++ examples/basic-usage/main.go
@@ -13,15 +13,15 @@ func main() {
 		os.Getenv("FLARE_API_KEY"),
 	)
 	resp, err := client.Get(
-		"/leaksdb/v2/sources", nil,
+		"/tokens/test", nil,
 	)
 	if err != nil {
-		fmt.Printf("failed to get sources: %s\n", err)
+		fmt.Printf("failed to test token: %s\n", err)
 		os.Exit(1)
 	}
 	defer resp.Body.Close()
 	if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
-		fmt.Printf("failed to print sources: %s\n", err)
+		fmt.Printf("failed to print response: %s\n", err)
 		os.Exit(1)
 	}
 }
