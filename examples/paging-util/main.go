9c29ca1499fca7f583961dfa45aa7bd34a6eae65
aviau <alexandre@alexandreviau.net>
2024-09-15 12:45:13 -0400

    examples: defer inside loop

--- examples/paging-util/main.go
+++ examples/paging-util/main.go
@@ -20,24 +20,26 @@ func main() {
 	for result, err := range client.IterGet(
 		"/leaksdb/v2/sources", nil,
 	) {
-		// Rate Limiting
-		time.Sleep(time.Second * 1)
+		func(result *flareio.IterResult, err error) {
+			// Rate Limiting
+			time.Sleep(time.Second * 1)
 
-		if err != nil {
-			fmt.Printf("unexpected error: %s\n", err)
-			os.Exit(1)
-		}
+			if err != nil {
+				fmt.Printf("unexpected error: %s\n", err)
+				os.Exit(1)
+			}
 
-		// Handle the response
-		// ...
+			// Handle the response...
+			defer result.Response.Body.Close()
 
-		// Print the status
-		fetchedPages = fetchedPages + 1
-		fmt.Printf(
-			"Fetched %d page(s) of LeaksDB Sources, next=%s\n",
-			fetchedPages,
-			result.Next,
-		)
+			// Print the status
+			fetchedPages = fetchedPages + 1
+			fmt.Printf(
+				"Fetched %d page(s) of LeaksDB Sources, next=%s\n",
+				fetchedPages,
+				result.Next,
+			)
+		}(result, err)
 	}
 
 }
