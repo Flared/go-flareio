ebbd0f842099aa44faddf4f2261da39d0e9772a3
aviau <alexandre@alexandreviau.net>
2024-09-15 12:36:19 -0400

    examples: add paging utils

---
+++ examples/paging-util/main.go
@@ -0,0 +1,43 @@
+//go:build go1.23
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/Flared/go-flareio"
+)
+
+func main() {
+	client := flareio.NewApiClient(
+		os.Getenv("FLARE_API_KEY"),
+	)
+
+	fetchedPages := 0
+
+	for result, err := range client.IterGet(
+		"/leaksdb/v2/sources", nil,
+	) {
+		// Rate Limiting
+		time.Sleep(time.Second * 1)
+
+		if err != nil {
+			fmt.Printf("unexpected error: %s\n", err)
+			os.Exit(1)
+		}
+
+		// Handle the response
+		// ...
+
+		// Print the status
+		fetchedPages = fetchedPages + 1
+		fmt.Printf(
+			"Fetched %d page(s) of LeaksDB Sources, next=%s\n",
+			fetchedPages,
+			result.Next,
+		)
+	}
+
+}
