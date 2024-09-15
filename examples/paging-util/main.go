4d0cedb422611816c51b70419ad4eb147676bdd5
aviau <alexandre@alexandreviau.net>
2024-09-15 12:54:34 -0400

    paging-util: remove inner func

--- examples/paging-util/main.go
+++ examples/paging-util/main.go
@@ -20,26 +20,24 @@ func main() {
 	for result, err := range client.IterGet(
 		"/leaksdb/v2/sources", nil,
 	) {
-		func(result *flareio.IterResult, err error) {
-			// Rate Limiting
-			time.Sleep(time.Second * 1)
+		// Rate Limiting
+		time.Sleep(time.Second * 1)
 
-			if err != nil {
-				fmt.Printf("unexpected error: %s\n", err)
-				os.Exit(1)
-			}
+		if err != nil {
+			fmt.Printf("unexpected error: %s\n", err)
+			os.Exit(1)
+		}
 
-			// Handle the response...
-			defer result.Response.Body.Close()
+		// Handle the response...
+		result.Response.Body.Close()
 
-			// Print the status
-			fetchedPages = fetchedPages + 1
-			fmt.Printf(
-				"Fetched %d page(s) of LeaksDB Sources, next=%s\n",
-				fetchedPages,
-				result.Next,
-			)
-		}(result, err)
+		// Print the status
+		fetchedPages = fetchedPages + 1
+		fmt.Printf(
+			"Fetched %d page(s) of LeaksDB Sources, next=%s\n",
+			fetchedPages,
+			result.Next,
+		)
 	}
 
 }
